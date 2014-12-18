78cd13dff57e3218fcbc2d2b92f21916f794bae9
mamod <mamod.mehyar@gmail.com>
2014-12-18 04:43:01 +0300

    breez template introducing a new template

---
+++ templates/breez/init.js
@@ -0,0 +1,16 @@
+
+//handlebars partials
+exports.partials = [
+	'handlebars/index.hbs'
+];
+
+//handlebars helpers
+exports.helpers = {
+	test : function(arg1){
+		console.log(arg1);
+		return 'TIIIIIIIIIIIIII';
+	}
+};
+
+//template data
+// exports.data = require('./data.json');
