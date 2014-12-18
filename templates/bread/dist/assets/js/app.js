2eba81293c7acbd3c4d5a009b538b0c9e7d139ca
mamod <mamod.mehyar@gmail.com>
2014-12-18 05:27:11 +0300

    update bread template

--- templates/bread/dist/assets/js/app.js
+++ templates/bread/dist/assets/js/app.js
@@ -1,5 +1,10 @@
 (function($){
     $(document).ready(function() {
-       //alert('sss'); 
+        //alert('sss'); 
+        $('.services .column').mouseenter(function(){
+            $(this).find('.icon').addClass('hover');
+        }).mouseleave(function(){
+            $(this).find('.icon').removeClass('hover');
+        });
     });
 }(jQuery));
---
+++ templates/bread/init.js
@@ -0,0 +1,13 @@
+
+//handlebars partials
+exports.partials = [
+	'handlebars/index.hbs'
+];
+
+//handlebars helpers
+exports.helpers = {
+	test : function(arg1){
+		console.log(arg1);
+		return 'TIIIIIIIIIIIIII';
+	}
+};
