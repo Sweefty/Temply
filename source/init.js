8808d9d26b3cc611adddf845225580a363501fec
mamod <mamod.mehyar@gmail.com>
2016-09-17 02:34:17 +0300

    update init.js file

--- source/init.js
+++ source/init.js
@@ -1,26 +1,26 @@
-//where to compile html pages, (dist) by default 
+//where to compile html pages, (dist) by default
 exports.outLocation = '';
 
 //handlebars object
 exports.handlebars = function(Handlebars){
 
-    //this gets some information about
-    //current compiled page
-    //current.location
-    //current.layout
-    //current.hbs
-    //current.page
-    //current.data
-    var current = exports.current;
-
-    //Handlebars.registerHelper('helper', helper);
+    // this gets some information about
+    // current compiled page
+    // current.location
+    // current.layout
+    // current.hbs
+    // current.page
+    // current.data
+    // Handlebars.registerHelper('helper', helper);
     // Handlebars.registerPartial('partial', partial);
+    var current = exports.current;
 };
+
 
 exports.data = {
     //define some javascript files to use with this project
     "javascript" : [],
-    
+
     //define fonts
     "fonts" : [
         "https://fonts.googleapis.com/css?family=Dosis",
@@ -29,4 +29,14 @@ exports.data = {
     //global options, add more options!
     "description" : "raw base template",
     "title" : "raw starting point template"
+};
+
+
+var fs = require('fs');
+exports.fileHandler = {
+    'html' : function(dir, content){
+        var content = fs.readFileSync(dir).toString('utf8');
+        return content;
+    },
+    // 'gif' : 'ignore'
 };
